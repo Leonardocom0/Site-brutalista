396d7e8194e1925c71e347a33a8938c29e4e21d8
Leonardocom0 <suponly666@gmail.com>
2025-05-24 03:16:50 -0300

    Site Brutalista

---
+++ script.js
@@ -0,0 +1,6 @@
+// Pequeno easter egg no console
+console.log('%cBem-vindo ao meu site brutalista!', 'color: black; background: white; font-size: 20px; padding: 5px;');
+
+document.addEventListener('DOMContentLoaded', () => {
+  alert('Bem-vindo ao meu site brutalista!');
+});
